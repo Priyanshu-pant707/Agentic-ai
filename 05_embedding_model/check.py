fe0a224bf6bb4455ad47b55c70518648a3cc30af
Priyanshu Pant <pantpriyanshu707@gmail.com>
2026-06-16 12:50:22 +0530

    Removed secret

---
+++ 05_embedding_model/check.py
@@ -0,0 +1,18 @@
+from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = InferenceClient(
+    api_key=os.getenv("HF_TOKEN")
+)
+
+response = client.chat.completions.create(
+    model="Qwen/Qwen2.5-7B-Instruct",
+    messages=[
+        {"role": "user", "content": "What is AI?"}
+    ]
+)
+
+print(response.choices[0].message.content)
